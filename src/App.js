a8bd08bdc925ae7793eaf8eddce655cc17fedb37
Ievgen Sein <xdiego.pwu@gmail.com>
2019-02-09 18:05:56 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = { mouseX: 0, mouseY: 0, mistakes: 0 };
+	}
+
+
+	handleMouseMove = event => {
+		this.setState({mouseX: event.clientX, mouseY: event.clientY})
+		if (this.state.mouseX > 300 || this.state.mouseY > 300) {
+			this.setState({mistakes: this.state.mistakes + 1})
+		}
+	}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    	<div style={{height: '750px'}} onMouseMove={this.handleMouseMove} className="bg-primary"> 
+     		<div> Current mouse position: {this.state.mouseX}, {this.state.mouseY} </div>
+     		<div> Mistakes: {parseInt(this.state.mistakes)} </div>
+    	</div>
     );
   }
 }
