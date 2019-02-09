4f74ea6746dc37664967496d3db409e75d3fd798
Ievgen Sein <xdiego.pwu@gmail.com>
2019-02-09 18:13:20 +0200

    beautify fix

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import React from 'react';
 class App extends React.Component {
 	constructor() {
 		super();
-		this.state = { mouseX: 0, mouseY: 0, mistakes: 0 };
+		this.state = {
+			mouseX: 0,
+			mouseY: 0,
+			mistakes: 0
+		};
 	}
 
-
 	handleMouseMove = event => {
-		this.setState({mouseX: event.clientX, mouseY: event.clientY})
-		if (this.state.mouseX > 300 || this.state.mouseY > 300) {
-			this.setState({mistakes: this.state.mistakes + 1})
+		this.setState( { mouseX: event.clientX, mouseY: event.clientY } )
+		if ( this.state.mouseX > 300 || this.state.mouseY > 300 ) {
+			this.setState( {
+				mistakes: this.state.mistakes + 1
+			} )
 		}
 	}
 
-  render() {
-    return (
-    	<div style={{height: '750px'}} onMouseMove={this.handleMouseMove} className="bg-primary"> 
-     		<div> Current mouse position: {this.state.mouseX}, {this.state.mouseY} </div>
-     		<div> Mistakes: {parseInt(this.state.mistakes)} </div>
-    	</div>
-    );
-  }
+	render() {
+		return ( <div style={{
+				height: '750px'
+			}} onMouseMove={this.handleMouseMove} className="bg-primary">
+			<div>
+				Current mouse position: {this.state.mouseX}, {this.state.mouseY}
+			</div>
+			<div>
+				Mistakes: {parseInt( this.state.mistakes )}
+			</div>
+		</div> );
+	}
 }
 
 export default App;
