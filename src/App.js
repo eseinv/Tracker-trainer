378bb922015110696352d97282bbb450a6128b36
Ievgen Sein <xdiego.pwu@gmail.com>
2019-02-14 00:16:19 +0200

    added game core

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div `
+	height: 750px;
+`
+
+const Playground = styled.div `
+	height: 400px;
+	border: ${ props => props.difficulty === 'easy'
+	? 60
+	: 30 }px solid #fff;
+`
 
 class App extends React.Component {
 	constructor() {
@@ -6,30 +18,74 @@ class App extends React.Component {
 		this.state = {
 			mouseX: 0,
 			mouseY: 0,
-			mistakes: 0
+			mistakes: 0,
+			difficulty: 'hell',
+			play: true
 		};
 	}
 
 	handleMouseMove = event => {
 		this.setState( { mouseX: event.clientX, mouseY: event.clientY } )
-		if ( this.state.mouseX > 300 || this.state.mouseY > 300 ) {
-			this.setState( {
-				mistakes: this.state.mistakes + 1
-			} )
+		if ( this.state.play ) {
+			//  left vertical
+			if ( this.state.mouseX > -1 && this.state.mouseX < 31 ) {
+				if ( this.state.mouseY < 80 || this.state.mouseY > 479 ) 
+					this.setState( {
+						mistakes: this.state.mistakes + 1
+					} )
+			}
+			// right vertical
+			if ( this.state.mouseX > 1504 && this.state.mouseX < 1536 ) {
+				if ( this.state.mouseY < 80 || this.state.mouseY > 479 ) 
+					this.setState( {
+						mistakes: this.state.mistakes + 1
+					} )
+			}
+			// above top
+			if ( this.state.mouseY < 80 ) {
+				this.setState( {
+					mistakes: this.state.mistakes + 1
+				} )
+			}
+			// middle block
+			if ( this.state.mouseY > 109 && this.state.mouseY < 450 ) {
+				if ( this.state.mouseX > 29 && this.state.mouseX < 1505 ) {
+					this.setState( {
+						mistakes: this.state.mistakes + 1
+					} )
+				}
+			}
+			// bellow bottom
+			if ( this.state.mouseY > 479 ) {
+				this.setState( {
+					mistakes: this.state.mistakes + 1
+				} )
+			}
 		}
 	}
 
+	handleDifficulty = event => {
+		this.setState( { difficulty: event.target.value } );
+	}
+
 	render() {
-		return ( <div style={{
-				height: '750px'
-			}} onMouseMove={this.handleMouseMove} className="bg-primary">
+		return ( <Wrapper className="bg-primary" onMouseMove={this.handleMouseMove}>
 			<div>
 				Current mouse position: {this.state.mouseX}, {this.state.mouseY}
 			</div>
-			<div>
-				Mistakes: {parseInt( this.state.mistakes )}
+			<div className="mt-2">
+				Difficulty:{' '}
+				<select onChange={this.handleDifficulty}>
+					<option>easy</option>
+					<option selected={this.state.difficulty === 'hell'
+							? true
+							: false}>hell</option>
+				</select>
 			</div>
-		</div> );
+			<Playground className="mt-4 bg-primary d-flex justify-content-center align-items-center" difficulty={this.state.difficulty}>
+				<h4>Mistakes: {parseInt( this.state.mistakes )}</h4>
+			</Playground>
+		</Wrapper> )
 	}
 }
 
