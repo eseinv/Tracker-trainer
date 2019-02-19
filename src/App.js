520068b2e3b108a40adb6c5ad162eedda8831499
Ievgen Sein <xdiego.pwu@gmail.com>
2019-02-19 15:17:02 +0200

    added start stop functionality

--- src/App.js
+++ src/App.js
@@ -12,6 +12,22 @@ const Playground = styled.div `
 	: 30 }px solid #fff;
 `
 
+const StartGameBox = styled.div `
+	height: 30px;
+	margin-top: 24px;
+	position: absolute;
+	color: #fff;
+	opacity: ${ props => props.playing
+	? '0.2'
+	: '1'}
+	&:hover {
+		cursor: ${ props => props.playing
+		? 'default'
+		: 'pointer' }
+	}
+
+`
+
 class App extends React.Component {
 	constructor() {
 		super();
@@ -20,34 +36,46 @@ class App extends React.Component {
 			mouseY: 0,
 			mistakes: 0,
 			difficulty: 'hell',
-			play: true
+			playing: false
 		};
 	}
 
 	handleMouseMove = event => {
 		this.setState( { mouseX: event.clientX, mouseY: event.clientY } )
-		if ( this.state.play ) {
-			//  left vertical
-			if ( this.state.mouseX > -1 && this.state.mouseX < 31 ) {
+		if ( this.state.playing ) {
+			//	left wall
+			if ( this.state.mouseX < 1 ) {
+				this.setState( {
+					mistakes: this.state.mistakes + 1
+				} )
+			}
+			// 	right wall
+			if ( this.state.mouseX > 1534 ) {
+				this.setState( {
+					mistakes: this.state.mistakes + 1
+				} )
+			}
+			//	left vertical
+			if ( this.state.mouseX > 0 && this.state.mouseX < 31 ) {
 				if ( this.state.mouseY < 80 || this.state.mouseY > 479 ) 
 					this.setState( {
 						mistakes: this.state.mistakes + 1
 					} )
 			}
-			// right vertical
+			//	right vertical
 			if ( this.state.mouseX > 1504 && this.state.mouseX < 1536 ) {
 				if ( this.state.mouseY < 80 || this.state.mouseY > 479 ) 
 					this.setState( {
 						mistakes: this.state.mistakes + 1
 					} )
 			}
-			// above top
+			//	above top
 			if ( this.state.mouseY < 80 ) {
 				this.setState( {
 					mistakes: this.state.mistakes + 1
 				} )
 			}
-			// middle block
+			//	middle block
 			if ( this.state.mouseY > 109 && this.state.mouseY < 450 ) {
 				if ( this.state.mouseX > 29 && this.state.mouseX < 1505 ) {
 					this.setState( {
@@ -55,7 +83,7 @@ class App extends React.Component {
 					} )
 				}
 			}
-			// bellow bottom
+			//	bellow bottom
 			if ( this.state.mouseY > 479 ) {
 				this.setState( {
 					mistakes: this.state.mistakes + 1
@@ -68,6 +96,15 @@ class App extends React.Component {
 		this.setState( { difficulty: event.target.value } );
 	}
 
+	toggleGame = () => {
+		this.setState( {
+			playing: !this.state.playing
+		} )
+		if ( !this.state.playing ) {
+			this.setState( { mistakes: 0 } )
+		}
+	}
+
 	render() {
 		return ( <Wrapper className="bg-primary" onMouseMove={this.handleMouseMove}>
 			<div>
@@ -75,13 +112,19 @@ class App extends React.Component {
 			</div>
 			<div className="mt-2">
 				Difficulty:{' '}
-				<select onChange={this.handleDifficulty}>
+				<select value={this.state.difficulty === 'hell'
+						? 'hell'
+						: 'easy'} onChange={this.handleDifficulty}>
 					<option>easy</option>
-					<option selected={this.state.difficulty === 'hell'
-							? true
-							: false}>hell</option>
+					<option>hell</option>
 				</select>
 			</div>
+			<StartGameBox className="bg-success d-flex align-items-center" onClick={this.toggleGame} playing={this.state.playing}>
+				{
+					this.state.playing
+						? '> Finish'
+						: '> Start here'
+				}</StartGameBox>
 			<Playground className="mt-4 bg-primary d-flex justify-content-center align-items-center" difficulty={this.state.difficulty}>
 				<h4>Mistakes: {parseInt( this.state.mistakes )}</h4>
 			</Playground>
